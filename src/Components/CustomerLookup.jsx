d842f38f1b870168f079b6efc66b9bb39ebf254d
JustinLGates <papagates1@gmail.com>
2020-08-24 21:27:47 -0600

    style modifications.

--- src/Components/CustomerLookup.jsx
+++ src/Components/CustomerLookup.jsx
@@ -1,7 +1,27 @@
 import React, { useState } from "react";
 import { api } from "../axios";
 
-let phoneNumber = "";
+let phoneNumber = ""; //(123)123-3456
+let formatPhoneNumber = (str) => {
+  let num = "";
+  for (let i = 0; i < str.length; i++) {
+    if (i == 0 && str.length > 3 && str[0] !== "(") {
+      num += "(";
+    }
+    if (i == 3 && str.length > 3 && str[4] != ")") {
+      num += ")";
+    }
+    if (i == 6 && str[6] != "-" && str.length > 6) {
+      num += "-";
+    }
+    num += str[i];
+    if (num.length >= 16) {
+      return num;
+    }
+  }
+  return num;
+};
+
 const CustomerLookUp = () => {
   const [customers, setcustomers] = useState([
     { id: 0, Name: "Justin", PhoneNumber: "2086954231" },
@@ -43,155 +63,204 @@ const CustomerLookUp = () => {
       >
         <h2 className="text-dark">Customer Lookup</h2>
         <div className="row d-flex justify-content-center">
-          <div className="col-4 col-md-3">
-            <button
-              className="p-2 px-3 my-1 p-md-4 px-md-5  num-box btn btn-dark "
-              value={1}
-              onClick={addToNumber}
-            >
-              1
-            </button>
-          </div>
-          <div className="col-4 col-md-3">
-            <button
-              className="p-2 px-3 my-1 p-md-4 text-large px-md-5 p-lg-5  num-box btn btn-dark "
-              value={2}
-              onClick={addToNumber}
-            >
-              2
-            </button>
-          </div>
-          <div className="col-4 col-md-3">
-            <button
-              className="p-2 px-3 my-1 p-md-4 text-large px-md-5 p-lg-5  num-box btn btn-dark "
-              value={3}
-              onClick={addToNumber}
-            >
-              3
-            </button>
-          </div>
-        </div>
-        <div className="row d-flex justify-content-center">
-          <div className="col-4 col-md-3">
-            <button
-              className="p-2 px-3 my-1 p-md-4 text-large px-md-5 p-lg-5  num-box btn btn-dark "
-              value={4}
-              onClick={addToNumber}
-            >
-              4
-            </button>
-          </div>
-          <div className="col-4 col-md-3">
-            <button
-              className="p-2 px-3 my-1 p-md-4 text-large px-md-5 p-lg-5  num-box btn btn-dark "
-              value={5}
-              onClick={addToNumber}
-            >
-              5
-            </button>
-          </div>
-          <div className="col-4 col-md-3">
-            <button
-              className="p-2 px-3 my-1 p-md-4 text-large px-md-5 p-lg-5  num-box btn btn-dark "
-              value={6}
-              onClick={addToNumber}
-            >
-              6
-            </button>
+          <div className="col-12 d-flex justify-content-center">
+            <div className="num-box m-2">
+              <button
+                className="num-box btn btn-dark "
+                value={1}
+                onClick={addToNumber}
+              >
+                1
+              </button>
+            </div>
+            <div className="num-box m-2">
+              <button
+                className="  text-large   num-box btn btn-dark "
+                value={2}
+                onClick={addToNumber}
+              >
+                2
+              </button>
+            </div>
+
+            <div className="num-box m-2">
+              <button
+                className=" text-large num-box btn btn-dark "
+                value={3}
+                onClick={addToNumber}
+              >
+                3
+              </button>
+            </div>
           </div>
         </div>
         <div className="row d-flex justify-content-center">
-          <div className="col-4 col-md-3">
-            <button
-              className="p-2 px-3 my-1 p-md-4 text-large px-md-5 p-lg-5  num-box btn btn-dark "
-              value={7}
-              onClick={addToNumber}
-            >
-              7
-            </button>
-          </div>
-          <div className="col-4 col-md-3">
-            <button
-              className="p-2 px-3 my-1 p-md-4 text-large px-md-5 p-lg-5  num-box btn btn-dark "
-              value={8}
-              onClick={addToNumber}
-            >
-              8
-            </button>
-          </div>
-          <div className="col-4 col-md-3">
-            <button
-              className="p-2 px-3 my-1 p-md-4 text-large px-md-5 p-lg-5  num-box btn btn-dark "
-              value={9}
-              onClick={addToNumber}
-            >
-              9
-            </button>
+          <div className="col-12 d-flex justify-content-center">
+            <div className="m-2">
+              <button
+                className=" text-large num-box btn btn-dark "
+                value={4}
+                onClick={addToNumber}
+              >
+                4
+              </button>
+            </div>
+
+            <div className="m-2">
+              <button
+                className=" my-1 p-md-4 text-large   num-box btn btn-dark "
+                value={5}
+                onClick={addToNumber}
+              >
+                5
+              </button>
+            </div>
+            <div className="m-2">
+              <button
+                className=" my-1 p-md-4 text-large   num-box btn btn-dark "
+                value={6}
+                onClick={addToNumber}
+              >
+                6
+              </button>
+            </div>
           </div>
         </div>
         <div className="row d-flex justify-content-center">
-          <div className="col-4 col-md-3">
-            <button
-              className="p-2 px-3 my-1 p-md-4 text-large px-md-5 p-lg-5  num-box btn btn-dark  "
-              onClick={removeLastDidget}
-            >
-              <i className="fas fa-long-arrow-alt-left"></i>
-            </button>
+          <div className="col-12  d-flex justify-content-center">
+            <div className="m-2">
+              <button
+                className="text-large num-box btn btn-dark "
+                value={7}
+                onClick={addToNumber}
+              >
+                7
+              </button>
+            </div>
+            <div className="m-2">
+              <button
+                className=" my-1 p-md-4 text-large   num-box btn btn-dark "
+                value={8}
+                onClick={addToNumber}
+              >
+                8
+              </button>
+            </div>
+            <div className="m-2">
+              <button
+                className=" my-1 p-md-4 text-large   num-box btn btn-dark "
+                value={9}
+                onClick={addToNumber}
+              >
+                9
+              </button>
+            </div>
           </div>
-          <div className="col-4 col-md-3">
-            <button
-              className="p-2 px-3 my-1 p-md-4 text-large px-md-5 p-lg-5  num-box btn btn-dark "
-              value={0}
-              onClick={addToNumber}
-            >
-              0
-            </button>
-          </div>
-          <div className="col-4 col-md-3">
-            <button
-              className="p-2 px-3 my-1 p-md-4 text-large px-md-5 p-lg-5  num-box btn btn-dark "
-              onClick={ClearNumber}
-            >
-              Clear
-            </button>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="col-12 d-flex justify-content-center">
+            <div className="m-2">
+              <button
+                className="text-large num-box btn btn-outline-secondary "
+                onClick={removeLastDidget}
+              >
+                <i className="fas fa-long-arrow-alt-left"></i>
+              </button>
+            </div>
+            <div className="m-2">
+              <button
+                className="text-large num-box btn btn-dark"
+                value={0}
+                onClick={addToNumber}
+              >
+                0
+              </button>
+            </div>
+            <div className="m-2">
+              <button
+                className="text-large  num-box btn-outline-danger  btn "
+                onClick={ClearNumber}
+              >
+                Clear
+              </button>
+            </div>
           </div>
         </div>
 
-        <div>
-          <input className="p-2 my-2 w-100" value={phoneNumberQuery} />
+        <div className="d-flex align-items-center justify-content-center">
+          <input
+            className="p-2 m-2"
+            value={formatPhoneNumber(phoneNumberQuery)}
+          />
+          <button className="btn btn-success m-2 p-2">New</button>
         </div>
-        <button className="btn btn-primary">Add new</button>
         <div className="text-left ">
           <h4 className=""> Customers</h4>
           <div className="box customer-list">
-            <div className="customer-listp-2 px-3 my-1 p-md-4 text-large px-md-5 p-lg-5 ">
+            <div className="customer-list my-1 p-md-4 text-large  ">
+              {customers.length > 0 ? (
+                <div className="row">
+                  <div className="col-4">
+                    <label>Name</label>
+                  </div>
+                  <div className="col-4">
+                    <label>Phone</label>
+                  </div>
+                </div>
+              ) : (
+                <div>No account matches {formatPhoneNumber(phoneNumber)}</div>
+              )}
               <div className="row">
-                <div className="col-4">
-                  <label>Name</label>
-                </div>
-                <div className="col-4">
-                  <label>Phone</label>
-                </div>
                 <div className="col-12">
                   <hr />
                 </div>
               </div>
-              {customers.map((customer, index) => {
-                return (
-                  <div
-                    key={index}
-                    className="row highlight"
-                    onClick={() => console.log("clicked", index)}
-                  >
-                    <div className="col-4">
-                      <label>{customer.Name}</label>
+              {customers.length > 0 ? (
+                customers.map((customer, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="row highlight"
+                      onClick={() => console.log("clicked", index)}
+                    >
+                      <div className="col-4">
+                        <label>{customer.Name}</label>
+                      </div>
+                      <div className="col-4">
+                        <label>{customer.PhoneNumber}</label>
+                      </div>
                     </div>
-                    <div className="col-4">
-                      <label>{customer.PhoneNumber}</label>
+                  );
+                })
+              ) : (
+                <div className="row">
+                  <div className="col-12">
+                    <label className="p-0 m-0" htmlFor="">
+                      Name
+                    </label>
+                    <br />
+                    <input type="text" placeholder="John doe" />
+                  </div>
+                  <br />
+                  <div className="col-12 pt-3">
+                    <label className="p-0 m-0">Phone number</label>
+                    <div className="col-12">
+                      <span className="border-bottom">
+                        {phoneNumber.length > 0 ? (
+                          <label className="text-dark">{phoneNumber}</label>
+                        ) : (
+                          <label className="text-secondary">
+                            {"(123)456-7890"}
+                          </label>
+                        )}
+                      </span>
                     </div>
                   </div>
-                );
-              })}
+                  <div className="col-12">
+                    <button className="fa fa-plus btn btn-primary"></button>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
