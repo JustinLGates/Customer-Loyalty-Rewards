3fd47d230c1a2063d93c931d87cf4bac7c2fe768
JustinLGates <papagates1@gmail.com>
2020-09-08 10:45:08 -0600

    pushing build to heroku

--- Setup.js
+++ Setup.js
@@ -3,7 +3,7 @@ import BaseController from "./server/utils/BaseController";
 
 export class Paths {
   static get Public() {
-    return __dirname + "/public/";
+    return __dirname + "/build";
   }
   static get Server() {
     return __dirname + "/server";
---
+++ build/precache-manifest.eb59bbaec199d879a926a227ab208f59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7090e07577054e1f0e9b895f8838fbfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf72d552fb299877bae8",
+    "url": "/static/css/main.724d8867.chunk.css"
+  },
+  {
+    "revision": "adc902bb1c202d54d607",
+    "url": "/static/js/2.6fa78ded.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.6fa78ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf72d552fb299877bae8",
+    "url": "/static/js/main.18d0139d.chunk.js"
+  },
+  {
+    "revision": "67fb421737c086b0686e",
+    "url": "/static/js/runtime-main.6d850c45.js"
+  },
+  {
+    "revision": "0ced1d1ea1c755eff40c54637e28e39e",
+    "url": "/static/media/buying.0ced1d1e.jpeg"
+  }
+]);
